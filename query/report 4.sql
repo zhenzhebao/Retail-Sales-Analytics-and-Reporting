255d4b3191e22ce4f51241dee97bc5aeed7a8906
Bao, Zhenzhe <zhenzhe.bao@uconn.edu>
2026-05-22 20:19:35 -0400

    add script for Report 4 Product Performance Report
    add script for Report 4 Product Performance Report

---
+++ query/report 4.sql
@@ -0,0 +1,17 @@
+-- Report 4 Product Performance Report
+select p.stockcode as 'Product Code',p.description as 'Product description',
+c.category_name as 'Category',d.product_department as 'Department',sum(ip.quantity) as 'Units sold',
+concat("$",round(sum(ip.quantity *p.unit_price),2)) as 'Total Sales', 
+count(distinct i.invoiceno) as 'Number of invoices containing the product'
+from product p
+join category c
+on c.category_id=p.category_id
+join department d
+on d.department_id=c.department_id
+join invoice_product ip 
+on ip.stockcode=p.stockcode
+join invoice i
+on i.invoiceno=ip.invoiceno
+where ip.quantity>0
+group by p.stockcode,p.description,c.category_name,d.product_department
+order by sum(ip.quantity) DESC;
